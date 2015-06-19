c2b5f820fe0c8b4610d826ec4931c2cd0a7fc082
randbw <apbward@gmail.com>
2015-06-19 14:40:09 +1000

    got the beginning part of the backbone router in

--- app/assets/javascripts/backbone/routers/router.js
+++ app/assets/javascripts/backbone/routers/router.js
@@ -1,1 +1,29 @@
 var app = app || {};
+
+app.AppRouter = Backbone.Router.extend({
+    routes: {
+        '': 'index'
+    }
+
+});
+
+// app.AppRouter = Backbone.Router.extend({
+//     routes: {
+//         '': 'index',
+//         'flights/:id': 'viewFlight'
+//     },
+
+//     index: function() {
+//         var appView = new app.AppView({
+//             collection: app.burningFlights
+//         });
+//         appView.render();
+//     },
+
+//     viewFlight: function(id) {
+//         app.flight = app.burningFlights.get(id);
+//         app.flightView = new app.FlightView({model : app.flight})
+//         app.flightView.render();
+//     }
+
+// });
