e2fe566cc1936c1bfd2f0558f338fb28eaec88e9
randbw <apbward@gmail.com>
2015-06-25 14:37:54 +1000

    going down the rabbit hole with feels updatR

--- app/assets/javascripts/backbone/utilities/updatefeels.js
+++ app/assets/javascripts/backbone/utilities/updatefeels.js
@@ -1,13 +1,26 @@
 var app = app || {};
 
 app.updateFeels = function () {
-  app.currentUserContact = app.currentUserContact || [];
-  if (app.currentUserContact.length > 0) {
-    for ( var i = 0 ; i < app.currentUserContact.length ; i++ ) {
-      app.contactEmails.push(app.currentUserContact[i].get("email_address"));
-    }
-  }
+  // highestFeel
+  // loop through all snippets where "result = positive" to find the highest "confidence" rating
+
+  // lowestFeel
+  // loop through all snippets where "result = negative" to find the lowest "confidence" rating
+
+  // currentFeel
+  // retrieve most recent snippet and find the result and confidence rating
+  
+  // weeksAverageFeel
+  // take all snippets from last week and run them through an algorithm which determines
+
   for ( var i = 0 ; i < app.currentUserContact.length ; i++ ) {
-    console.log('ok')
+    // feelz variables
+    var highestFeel = 0;
+    var lowestFeel = 0;
+    var currentFeel = 0;
+    var weeksAverageFeel = 0;
+
+    console.log(app.currentUserContact[i].attributes.id);
+      
   }
-}
+};
