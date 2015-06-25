b0700ff78015eeef01be411c3e1c22feab833a89
randbw <apbward@gmail.com>
2015-06-25 15:02:42 +1000

    rewrote function to remove redefining variables everytime the for loop ran. i need a good sleep

--- app/assets/javascripts/backbone/utilities/updatefeels.js
+++ app/assets/javascripts/backbone/utilities/updatefeels.js
@@ -1,26 +1,20 @@
 var app = app || {};
 
 app.updateFeels = function () {
-  // highestFeel
+  var highestFeel = 0;
   // loop through all snippets where "result = positive" to find the highest "confidence" rating
-
-  // lowestFeel
+  var lowestFeel = 0;
   // loop through all snippets where "result = negative" to find the lowest "confidence" rating
-
-  // currentFeel
+  var currentFeel = 0;
   // retrieve most recent snippet and find the result and confidence rating
-  
-  // weeksAverageFeel
+  var weeksAverageFeel = 0;
   // take all snippets from last week and run them through an algorithm which determines
-
+  var allSnipzzz = [];
+  
   for ( var i = 0 ; i < app.currentUserContact.length ; i++ ) {
-    // feelz variables
-    var highestFeel = 0;
-    var lowestFeel = 0;
-    var currentFeel = 0;
-    var weeksAverageFeel = 0;
-
-    console.log(app.currentUserContact[i].attributes.id);
-      
+    // Find all snippets belonging to each of the user's contacts
+    var snip = app.allSnippets.where({contact_id: app.currentUserContact[i].attributes.id});
+    allSnipzzz.push(snip);
   }
+  console.log(allSnipzzz);
 };
