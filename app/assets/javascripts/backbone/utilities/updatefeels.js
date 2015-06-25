c6a86e607d6a536b01fd3e566aaeb8041a13ddab
randbw <apbward@gmail.com>
2015-06-25 15:44:59 +1000

    getting rid of for loops for backbone functions

--- app/assets/javascripts/backbone/utilities/updatefeels.js
+++ app/assets/javascripts/backbone/utilities/updatefeels.js
@@ -10,11 +10,40 @@ app.updateFeels = function () {
   var weeksAverageFeel = 0;
   // take all snippets from last week and run them through an algorithm which determines
   var allSnipzzz = [];
-  
+
   for ( var i = 0 ; i < app.currentUserContact.length ; i++ ) {
-    // Find all snippets belonging to each of the user's contacts
+
+    // Array of all snippets belonging to each of the user's contacts
     var snip = app.allSnippets.where({contact_id: app.currentUserContact[i].attributes.id});
-    allSnipzzz.push(snip);
+    debugger;
+    // inception time
+    for (var x = 0; x < snip.length; x++) {
+
+      // Turn snippet.context into an object
+      s = JSON.parse(snip[x].attributes.context);
+
+      // Change confidence into integer for comparing
+      confidence = parseInt(s.confidence);
+
+      // Determine whether snippet is positive or negative
+      if (s.result === 'Positive') {
+        if (confidence > highestFeel) {
+          highestFeel = confidence
+        }
+
+      } else if (s.result === 'Negative') {
+        if (confidence > lowestFeel) {
+          lowestFeel = confidence
+        }
+        
+      } else {
+        return
+      }
+      debugger;
+    }
+    // allSnipzzz.push(snip);
   }
-  console.log(allSnipzzz);
+  
+  console.log('Highest Feel:',highestFeel,'Lowest Feel:', lowestFeel);
+  // console.log(allSnipzzz);
 };
