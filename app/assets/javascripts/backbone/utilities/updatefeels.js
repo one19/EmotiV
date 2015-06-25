decab32f90fea54bf18f70827159b53dce84daa4
randbw <apbward@gmail.com>
2015-06-25 16:55:10 +1000

    one step closer to glory

--- app/assets/javascripts/backbone/utilities/updatefeels.js
+++ app/assets/javascripts/backbone/utilities/updatefeels.js
@@ -1,47 +1,31 @@
 var app = app || {};
 
 app.updateFeels = function () {
-  var highestFeel = 0;
+
   // loop through all snippets where "result = positive" to find the highest "confidence" rating
-  var lowestFeel = 0;
+  var highestFeel = 0;
   // loop through all snippets where "result = negative" to find the lowest "confidence" rating
-  var currentFeel = 0;
+  var lowestFeel = 0;
   // retrieve most recent snippet and find the result and confidence rating
-  var weeksAverageFeel = 0;
+  var currentFeel = 0;
   // take all snippets from last week and run them through an algorithm which determines
-  var allSnipzzz = [];
+  var weeksAverageFeel = 0;
 
   for ( var i = 0 ; i < app.currentUserContact.length ; i++ ) {
 
     // Array of all snippets belonging to each of the user's contacts
     var snip = app.allSnippets.where({contact_id: app.currentUserContact[i].attributes.id});
+    // Array of all snippets where result is negative
+    var snipNeg = _.filter( snip, function (snippet) {return JSON.parse(snippet.attributes.context).result === "Negative"});
+    // snipNeg.each
+    var snipPos = _.filter( snip, function (snippet) {return JSON.parse(snippet.attributes.context).result === "Positive"});
     debugger;
-    // inception time
-    for (var x = 0; x < snip.length; x++) {
 
       // Turn snippet.context into an object
-      s = JSON.parse(snip[x].attributes.context);
-
+      // s = JSON.parse(snip[x].attributes.context);
       // Change confidence into integer for comparing
-      confidence = parseInt(s.confidence);
-
-      // Determine whether snippet is positive or negative
-      if (s.result === 'Positive') {
-        if (confidence > highestFeel) {
-          highestFeel = confidence
-        }
-
-      } else if (s.result === 'Negative') {
-        if (confidence > lowestFeel) {
-          lowestFeel = confidence
-        }
-        
-      } else {
-        return
-      }
-      debugger;
-    }
-    // allSnipzzz.push(snip);
+      // confidence = parseInt(s.confidence);
+      
   }
   
   console.log('Highest Feel:',highestFeel,'Lowest Feel:', lowestFeel);
