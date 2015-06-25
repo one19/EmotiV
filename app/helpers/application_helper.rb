5f415cc0c5a407291efb7ccb98f61d04953da4b9
Charlotte Hicks <charlotte@Charlottes-MacBook-Pro.local>
2015-06-25 11:27:29 +1000

    nav bar stuff

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,7 +8,7 @@ module ApplicationHelper
     end
     if @current_user.present?
       nav += '<li class="nav">' + link_to("Edit profile", edit_user_path(@current_user)) + '</li>'
-      nav += '<li class="nav usersname">' + link_to("Log out #{ @current_user.name }", login_path, :method => :delete) + '</li>'
+      nav += '<li class="nav" id="usersname">' + link_to("Log out #{ @current_user.name }", login_path, :method => :delete) + '</li>'
     else
       nav += '<a id="modal_trigger" href="#modal" class="nav">Log In <span id="small-or">OR</span> Sign Up</a>'
     end
