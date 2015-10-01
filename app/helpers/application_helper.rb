569fe826f65ac84e3b6129c915e1bd032727ec5b
randbw <apbward@gmail.com>
2015-10-02 00:01:35 +1000

    changed home.css to scss. edited the nav to include title of page. removed commented out code. added some paragraphs to better explain the site on the home page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,6 @@ module ApplicationHelper
 
   def funkynav
     nav = ''
-
-    if @current_user.present? && @current_user.admin?
-      
-    end
     if @current_user.present?
       nav += '<li class="nav">' + link_to("Edit profile", edit_user_path(@current_user)) + '</li>'
       nav += '<li class="nav" id="usersname">' + link_to("Log out #{ @current_user.name }", login_path, :method => :delete) + '</li>'
@@ -13,7 +9,7 @@ module ApplicationHelper
       nav += '<a id="modal_trigger" href="#modal" class="nav">Log In <span id="small-or">OR</span> Sign Up</a>'
     end
 
-    nav += '<a href="/"><div id="logobg"><h3>LOGO</h3><i class="icon-chat"></i></div></a>'
+    nav += '<a href="/"><div id="logobg"><h3>LOGO</h3><i class="icon-chat"></i></div></a> <span id="headerName"><a href="/">!(SheLovesMe)</a></span>'
 
     nav
   end
